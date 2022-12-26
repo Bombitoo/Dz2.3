401e480db4a52c4123d8481d957bac43cfae1ef7
Bombitoo <78532608+Bombitoo@users.noreply.github.com>
2022-12-26 22:48:44 +0300

    Add files via upload

---
+++ main2.3.cpp
@@ -0,0 +1,19 @@
+#include (studio.h)
+
+main()
+{
+  int pin1,pin2;
+  printf("pin1=");
+  scanf("%i",&pin1);
+  printf("pin2=");
+  scanf("%i",&pin2=2);  
+  
+  if((pin1=223)&&(pin2=322))||((pin1=445)&&(pin2=554))
+  {
+     printf("OYKE");                       
+  }  
+  else
+  {
+     printf("OhNOOO");
+  }  
+}
